05c8e593ea6afe2c5c976b585e18328a712607b2
priyambada <spriyambada2369@gmail.com>
2021-06-30 08:36:26 +0530

    bug fix

--- Component/array/TestimonialData.jsx
+++ Component/array/TestimonialData.jsx
@@ -11,13 +11,13 @@ const TestimonialData =[
         skill:"Angular Developer",
         Pline1:"I found this organization and joined it with the help of one of my closest friend. After joining MoSahay we had joined a team with a mentor in it. I was working in MoSahay on the Designing team. The good thing there was we were free to pick any of the technology to work. I contributed there in Frontend and apart from that, we did various things like RPA, VIX, Word Press, etc"
     },
-    {
-        image:"./3.jpg",
-        uName:"Jarry",
-        skill:"owner",
-        Pline1:"I am at ane where I just want to be fit and healthy our bodies are our resposibility!So start caring for your body and it will care for you.Eat clean it will care for you and workout hard."
-    },
-    
+    // {
+    //     image:"./3.jpg",
+    //     uName:"Jarry",
+    //     skill:"owner",
+    //     Pline1:"I am at ane where I just want to be fit and healthy our bodies are our resposibility!So start caring for your body and it will care for you.Eat clean it will care for you and workout hard."
+    // },
+     
 ];
 
 export default TestimonialData;
