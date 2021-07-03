1075e6486fbc5cd994f6b4f34d8c6a8782ea6996
priyambada <spriyambada2369@gmail.com>
2021-07-03 19:36:59 +0530

    bug fix

--- Component/parentsComnents/FilterJob.jsx
+++ Component/parentsComnents/FilterJob.jsx
@@ -161,7 +161,7 @@ export default function FilterJob() {
 <div className={styles.radioBtn2}>
               <FormControl component="fieldset">
                 <FormLabel component="legend">
-                  <b>Shorted By Data</b>
+                  <b>Sorted By Date</b>
                 </FormLabel>
                 <RadioGroup
                   aria-label="shorting"
