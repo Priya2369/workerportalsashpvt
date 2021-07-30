5a8fb2aebee4b749efd299e19517f06f12c95429
priyambada <spriyambada2369@gmail.com>
2021-07-30 17:27:52 +0530

    fixed profile bug

--- Component/parentsComnents/ContactUs.jsx
+++ Component/parentsComnents/ContactUs.jsx
@@ -4,7 +4,7 @@ import{ faEnvelope, } from '@fortawesome/free-regular-svg-icons';
 import{ faMap} from '@fortawesome/free-regular-svg-icons';
 import { toast } from "react-toastify";
 
-import emailjs from 'emailjs-com'
+// import emailjs from 'emailjs-com'
 
 
 
@@ -19,30 +19,30 @@ import { faMarker } from '@fortawesome/free-solid-svg-icons';
 toast.configure();
 
 export default function ContactUs() {
-    const [detail, setDetail] = useState({
-        name:'',
-        email:'',
-        No:'',
-        message:'',
-        })
+    // const [detail, setDetail] = useState({
+    //     name:'',
+    //     email:'',
+    //     No:'',
+    //     message:'',
+    //     })
 
        function submit(e){
            e.preventDefault();
-           console.log(e.target.value)
-           emailjs.sendForm('service_sfm7kik','template_mt9b19e',e.target,"user_NxU5uNQCWbHXT7BrIW4r3")
-           .then(res=>{
-            toast.success("Email sent sucessfully", {
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                position: "bottom-right",
-                autoClose: 5000,
-              });
+        //    console.log(e.target.value)
+        //    emailjs.sendForm('service_sfm7kik','template_mt9b19e',e.target,"user_NxU5uNQCWbHXT7BrIW4r3")
+        //    .then(res=>{
+        //     toast.success("Email sent sucessfully", {
+        //         hideProgressBar: true,
+        //         closeOnClick: true,
+        //         pauseOnHover: true,
+        //         draggable: true,
+        //         progress: undefined,
+        //         position: "bottom-right",
+        //         autoClose: 5000,
+        //       });
 
-               console.log(res);
-           }).catch(error=>(console.log(error)))
+        //        console.log(res);
+        //    }).catch(error=>(console.log(error)))
            
     
 
